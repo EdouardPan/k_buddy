b84642e466d67acb9aeec803bce050cc9b56082b
Edouard Panossian <edouard.panossian@gmail.com>
2018-05-28 15:59:42 +0200

    [ADD] Routes defined

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'pages/adding_menu', to: 'pages#adding_menu'
+
+  resources :medical_records
+  resources :symptoms
+  resources :appointments
+  resources :prescriptions
+  resources :calendar, only: [:index, :show]
 end
