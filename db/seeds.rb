cfc050d4397f3914dea9c2d044036bf8fcacfb71
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-05-29 17:30:06 +0200

    Edited index page for Medical Records, sorted by reverse chronological order, established links to show

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,29 @@ puts 'Creating medical records...'
     user_id: "1"
   )
   medical_record_1.save!
+
+  medical_record_2 = MedicalRecord.new(
+    record_date: DateTime.new(2017,10,14,8),
+    title: "record 2",
+    category: "Imagerie",
+    user_id: "1"
+  )
+  medical_record_2.save!
+
+  medical_record_3 = MedicalRecord.new(
+    record_date: DateTime.new(2017,11,14,8),
+    title: "record 3",
+    category: "Prise de Sang",
+    user_id: "1"
+  )
+  medical_record_3.save!
+
+  medical_record_4 = MedicalRecord.new(
+    record_date: DateTime.new(2017,12,14,8),
+    title: "record 4",
+    category: "Compte Rendu",
+    user_id: "1"
+  )
+  medical_record_4.save!
 puts 'Finished!'
 
