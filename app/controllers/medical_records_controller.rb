ef7a3758c709483ab0eeebb4bc9b241b11ebb2e3
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-05-29 18:53:45 +0200

    Added Medical Record methods in controller, Added figaro and Cloudinary own api key, Added MR form

--- app/controllers/medical_records_controller.rb
+++ app/controllers/medical_records_controller.rb
@@ -1,4 +1,5 @@
 class MedicalRecordsController < ApplicationController
+  before_action :set_medical_records, only: [:show, :edit, :update, :destroy]
 
   def index
     @medical_records = policy_scope(MedicalRecord)
@@ -10,12 +11,21 @@ class MedicalRecordsController < ApplicationController
   end
 
   def new
+    @medical_record = MedicalRecord.new
     authorize @medical_record
   end
 
   def create
+    @medical_record = MedicalRecord.new(medical_record_params)
     authorize @medical_record
+    @medical_record.user = current_user
+    if @medical_record.save
+      redirect_to medical_record_path(@medical_record)
+    else
+      render :new
+    end
   end
+
 
   def edit
     authorize @medical_record
@@ -28,4 +38,15 @@ class MedicalRecordsController < ApplicationController
   def destroy
     authorize @medical_record
   end
+
+  private
+
+  def set_medical_records
+    @medical_record = MedicalRecord.find(params[:id])
+  end
+
+  def medical_record_params
+    params.require(:medical_record).permit(:record_date, :title, :category, :description, {photos: []})
+  end
+
 end
