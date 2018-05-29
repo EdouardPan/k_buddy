4ef96d485b85848506c1bb6100cdc1cde1e7c2a8
Edouard Panossian <edouard.panossian@gmail.com>
2018-05-29 18:53:36 +0200

    [ADD] Calendar with preliminary design

--- app/javascript/components/calendar.js
+++ app/javascript/components/calendar.js
@@ -7,7 +7,42 @@ export function showCalendar() {
 
   $('#calendar').fullCalendar({
     // put your options and callbacks here
-     weekends: false // will hide Saturdays and Sundays
+    defaultView: 'month',
+    locale: 'fr',
+    weekends: false, // will hide Saturdays and Sundays
+    height: 540,
+    dayClick: function() {
+     alert('To be customed to add info');
+    },
+    header: {
+      left:   'month, agendaWeek, agendaDay',
+      center: '',
+      right:  'prev,next'
+    },
+    eventSources: [
+
+        // your event source
+        {
+        events: [
+          {
+            title  : 'event1',
+            start  : '2018-05-30'
+          },
+          {
+            title  : 'event2',
+            start  : '2018-05-20',
+            end    : '2018-05-21'
+          },
+          {
+            title  : 'event3',
+            start  : '2018-06-01T12:30:00',
+            allDay : false // will make the time show
+          }
+        ],
+        color: 'black',     // an option!
+        textColor: 'yellow' // an option!
+      }
+    ]
   })
 
 }
