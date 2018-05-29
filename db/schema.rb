9919ac60f390e73e10d37f9b5bcb74a6b8b50d35
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-05-29 16:06:21 +0200

    Changed type to category in medical_records & Created 1 simple seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_29_125232) do
+ActiveRecord::Schema.define(version: 2018_05_29_135257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 2018_05_29_125232) do
   create_table "medical_records", force: :cascade do |t|
     t.date "record_date"
     t.string "title"
-    t.string "type"
+    t.string "category"
     t.text "description"
     t.bigint "user_id"
     t.datetime "created_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ puts 'Creating medical records...'
   medical_record_1 = MedicalRecord.new(
     record_date: DateTime.new(2017,9,14,8),
     title: "record 1",
-    type: "CR",
+    category: "compte rendu",
     user_id: "1"
   )
   medical_record_1.save!
