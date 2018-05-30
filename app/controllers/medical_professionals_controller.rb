326fa91b9c2e609ea00d87f2088643b454c9b3ea
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-05-30 19:11:07 +0200

    [ADD] the event_type parameter to help us redirect the form towards either create appt or create prescription

--- app/controllers/medical_professionals_controller.rb
+++ app/controllers/medical_professionals_controller.rb
@@ -9,12 +9,23 @@ class MedicalProfessionalsController < ApplicationController
   end
 
   def new
+    @event_type = params[:event_type]
     @medical_professional = MedicalProfessional.new
     authorize @medical_professional
   end
 
   def create
+    @medical_professional = MedicalProfessional.new
     authorize @medical_professional
+    if @medical_professional.save
+      if params[:event_type] == "appointment"
+        redirect_to new_medical_professional_appointment_path(@medical_professional)
+      else
+        redirect_to new_medical_professional_prescription_path(@medical_professional)
+      end
+    else
+      render :new
+    end
   end
 
   def edit
