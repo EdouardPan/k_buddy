6c49bdfaf99ac7a8cae4b1b93f2a55bba08594b6
Andrew Stearns <andrew.stearns@sciencespo.fr>
2018-05-30 11:05:15 +0200

    navbar still in progress

--- app/javascript/components/addmenu.js
+++ app/javascript/components/addmenu.js
@@ -4,14 +4,8 @@ const addButton = document.getElementById("navbar-plus-icon");
 
 
 function menuVisible() {
-  addButton.click(function() {
-    console.log("Hello");
-    addMenu.slideUp(1500);
-  });
-  //addButton.addEventListener("click", function() {
-  //  addMenu.style.visibility = "visible";
-  //addMenu.slideToggle(1500);
-  //});
+  addButton.addEventListener("click", function() {
+    addMenu.style.visibility = "visible";
   }
 
 
