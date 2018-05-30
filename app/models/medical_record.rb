5424f88a78ae119a5a96759b33f37d4462e5c915
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-05-30 11:55:58 +0200

    [EDIT] icons html added png extension & added inclusion in MR model

--- app/models/medical_record.rb
+++ app/models/medical_record.rb
@@ -2,7 +2,7 @@ class MedicalRecord < ApplicationRecord
   belongs_to :user
 
   validates :record_date, presence: true
-  validates :category, presence: true
+  validates :category, inclusion: { in: ["Consultation Médicale", "Opération", "Vaccin", "Maladie Infantile", "Antécédent Familial", "Prises de Sang", "Autres"] }
 
   mount_uploaders :photos, PhotoUploader
 end
