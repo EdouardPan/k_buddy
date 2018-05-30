b2c5f813cc3826f796bbbea39a52b473370e8622
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-05-30 13:09:17 +0200

    [EDIT] MR index page

--- app/models/medical_record.rb
+++ app/models/medical_record.rb
@@ -2,7 +2,7 @@ class MedicalRecord < ApplicationRecord
   belongs_to :user
 
   validates :record_date, presence: true
-  validates :category, inclusion: { in: ["Consultation Médicale", "Opération", "Vaccin", "Maladie Infantile", "Antécédent Familial", "Prises de Sang", "Autres"] }
+  validates :category, inclusion: { in: ["Consultation Médicale", "Opération", "Vaccin", "Maladie Infantile", "Antécédent Familial", "Prise de Sang", "Autre"] }
 
   mount_uploaders :photos, PhotoUploader
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,33 +2,33 @@
 
 puts 'Creating medical records...'
   medical_record_1 = MedicalRecord.new(
-    record_date: DateTime.new(2017,9,14,8),
-    title: "record 1",
-    category: "Compte Rendu",
+    record_date: DateTime.new(2012,9,14,8),
+    title: "Pneumothorax",
+    category: "Opération",
     user_id: "1"
   )
   medical_record_1.save!
 
   medical_record_2 = MedicalRecord.new(
-    record_date: DateTime.new(2017,10,14,8),
-    title: "record 2",
-    category: "Imagerie",
+    record_date: DateTime.new(2009,10,14,8),
+    title: "Eczéma",
+    category: "Antécédent Familial",
     user_id: "1"
   )
   medical_record_2.save!
 
   medical_record_3 = MedicalRecord.new(
-    record_date: DateTime.new(2017,11,14,8),
-    title: "record 3",
+    record_date: DateTime.new(2012,11,14,8),
+    title: "Examen Prise de Sang 2012",
     category: "Prise de Sang",
     user_id: "1"
   )
   medical_record_3.save!
 
   medical_record_4 = MedicalRecord.new(
-    record_date: DateTime.new(2017,12,14,8),
-    title: "record 4",
-    category: "Compte Rendu",
+    record_date: DateTime.new(2010,12,14,8),
+    title: "Consultation ORL",
+    category: "Autre",
     user_id: "1"
   )
   medical_record_4.save!
