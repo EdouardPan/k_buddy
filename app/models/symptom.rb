e3a6282d0bfb4250ced74cc64d551230ce0e1aa4
Floriane Perrin de Brichambaut <floperrindb@gmail.com>
2018-05-31 15:44:10 +0200

    [CREATE] show and index page for the symptoms

--- app/models/symptom.rb
+++ app/models/symptom.rb
@@ -1,6 +1,14 @@
 class Symptom < ApplicationRecord
   belongs_to :user
 
+  include PgSearch
+
+  pg_search_scope :search_symptom,
+    against: [ :name, :start_date, :end_date, :intensity, :description ],
+    using: {
+      tsearch: { prefix: true }
+    }
+
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :intensity, inclusion: { in: (0..10).to_a }
