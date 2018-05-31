79f3b0e34f247daed8759d366d0071e7b013a8ac
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-05-31 18:39:54 +0200

    [EDIT] Show pages & Edit multiple photos

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,8 @@ import { menuInvisible } from '../components/menuinvisible';
 import $ from 'jquery';
 import { showCalendar } from '../components/calendar';
 
+import { modal } from '../components/modal';
+
 
 if (document.getElementById('calendar')){
   $(document).ready(showCalendar());
@@ -11,3 +13,6 @@ if (document.getElementById('calendar')){
 menuVisible();
 menuInvisible();
 
+if (document.querySelector('.photo-box')){
+  modal();
+}
