9abbc4896c584d7d5b5f5bd94609e75e78cb5c74
Floriane Perrin de Brichambaut <floperrindb@gmail.com>
2018-06-04 17:21:59 +0200

    update taken treatment / JS

--- app/controllers/treatments_controller.rb
+++ app/controllers/treatments_controller.rb
@@ -6,15 +6,17 @@ class TreatmentsController < ApplicationController
   end
 
   def update
-    # raise
     treatment = Treatment.find(params[:id])
     authorize treatment
 
     # treatment.taken = true
     # treatment.save!
 
-    treatment.update!(taken: true)
-
-    redirect_to treatment_path(treatment)
+    if treatment.update!(taken: true)
+      respond_to do |format|
+        format.html {redirect_to treatment_path(treatment)}
+        format.js
+      end
+    end
   end
 end
