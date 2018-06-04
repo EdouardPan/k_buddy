cac3ec2877e714a86f77b9ab6f0fddaa43018b69
Andrew Stearns <andrew.stearns@sciencespo.fr>
2018-06-04 12:32:40 +0200

    autocomplete works

---
+++ app/javascript/components/autocomplete.js
@@ -0,0 +1,27 @@
+import 'algoliasearch';
+import  autocomplete  from 'autocomplete.js';
+
+// Without '' or {}, it is a module that we are importing.
+
+export function algoliaAutocomplete() {
+  console.log("Hello")
+  var client = algoliasearch('7K6EPIOZJD', '43c96c2020a8f0a9229a8ab31cd59e19');
+  var index = client.initIndex('Drug');
+  console.log(index);
+  autocomplete('#search-input', { hint: false }, [
+    {
+      source: autocomplete.sources.hits(index, { hitsPerPage: 5 }),
+      displayKey: 'name',
+      templates: {
+        suggestion: function(suggestion) {
+          return suggestion._highlightResult.name.value;
+        }
+      }
+    }
+  ]).on('autocomplete:selected', function(event, suggestion, dataset) {
+    console.log(suggestion, dataset);
+  });
+}
+
+algoliaAutocomplete();
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,15 +1,15 @@
-//= require algolia/v3/algoliasearch.min
-
 import "bootstrap";
 import { menuVisible } from '../components/addmenu';
 import { menuInvisible } from '../components/menuinvisible';
 import $ from 'jquery';
+
 import { showCalendar } from '../components/calendar';
 
 import { modal } from '../components/modal';
 
 import { openNav } from '../components/temp';
 import { closeNav } from '../components/temp';
+import { algoliaAutocomplete } from '../components/autocomplete';
 
 if (document.getElementById('calendar')){
   $(document).ready(showCalendar());
