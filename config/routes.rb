ef1141f75b102d471fe9a22335620047071b8f08
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-06-05 13:54:54 +0200

    Added destroy method for pictures

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   resources :prescriptions, except: [:new, :create]
   resources :calendar, only: [:index, :show]
   resources :treatments, only: [:show, :update]
+  resources :photos, only: [:destroy]
 end
