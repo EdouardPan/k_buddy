fb32bf7d0c69ce0ea7e59a77acf47dfdb5ab6e7a
Andrew Stearns <andrew.stearns@sciencespo.fr>
2018-06-05 09:11:00 +0200

    not sure what this is

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,8 @@ import { showCalendar } from '../components/calendar';
 import { modal } from '../components/modal';
 
 
-import { openNav } from '../components/temp';
-import { closeNav } from '../components/temp';
+//import { openNav } from '../components/temp';
+//import { closeNav } from '../components/temp';
 import { algoliaAutocomplete } from '../components/autocomplete';
 import { openNav } from '../components/sidenav';
 import { closeNav } from '../components/sidenav';
