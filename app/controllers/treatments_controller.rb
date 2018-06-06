f483e24b744658b904417a8b926217c389b0d595
Andrew Stearns <andrew.stearns@sciencespo.fr>
2018-06-06 18:32:42 +0200

    Checkbox button woderous

--- app/controllers/treatments_controller.rb
+++ app/controllers/treatments_controller.rb
@@ -7,15 +7,15 @@ class TreatmentsController < ApplicationController
   end
 
   def update
-    treatment = Treatment.find(params[:id])
-    authorize treatment
+    @treatment = Treatment.find(params[:id])
+    authorize @treatment
 
     # treatment.taken = true
     # treatment.save!
 
-    if treatment.update!(taken: true)
+    if @treatment.update!(taken: true)
       respond_to do |format|
-        format.html {redirect_to treatment_path(treatment)}
+        format.html {redirect_to treatment_path(@treatment)}
         format.js
       end
     end
